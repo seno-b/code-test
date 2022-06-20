22888e4b92ffd442c83d7c7a8ef5a1398be44407
sino <sino@peachy.run>
2022-06-20 22:40:13 +0900

    feat: 카카오 모빌리티 코테 엉엉

---
+++ src/kakao/java/mobility/Task2.java
@@ -0,0 +1,14 @@
+package kakao.java.mobility;
+
+public class Task2 {
+
+  public static int solution(int[] T, int[] A) {
+    // write your code in Java SE 8
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    solution(new int[]{}, new int[]{});
+  }
+
+}
